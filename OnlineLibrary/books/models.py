bee099bcd11f5d8af403521cc67f6299d211a0d1
RaDmAn2222 <radmanhabibi2222@gmail.com>
2024-06-12 22:53:42 +0330

    adding the image paths to the database for future usages

--- OnlineLibrary/books/models.py
+++ OnlineLibrary/books/models.py
@@ -9,6 +9,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.title
