7a09562a48944fcdad337343d8facc3acfeaff6f
RaDmAn2222 <radmanhabibi2222@gmail.com>
2024-07-02 15:41:58 +0330

    fixing an issue in the database and adding pdf_url column to it. Also, finishing books template and static

--- OnlineLibrary/books/models.py
+++ OnlineLibrary/books/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     image_url = models.URLField(max_length=200, null=True, blank=True)
+    pdf_url = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
