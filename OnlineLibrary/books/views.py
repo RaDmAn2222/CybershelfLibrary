66259e72faaa5f89af4af659a2edaaab82976e37
RaDmAn2222 <radmanhabibi2222@gmail.com>
2024-07-07 13:35:42 +0330

    adding the search bar and necessary things into the view.py and fixing some issues and also modifying some image_urls in database

--- OnlineLibrary/books/views.py
+++ OnlineLibrary/books/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Book, Author
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def books(request):
-    books = Book.objects.select_related('author').all()
+    query = request.GET.get('q')
+    if query:
+        books_list = Book.objects.filter(
+            Q(title__icontains=query) |
+            Q(author__name__icontains=query)
+        ).select_related('author')
+    else:
+        books_list = Book.objects.select_related('author').all()
+        
+    paginator = Paginator(books_list, 24)  # Show 24 books per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = loader.get_template("books.html")
     context = {
-        "books": books,
+        "page_obj": page_obj,
+        "query": query,
     }
     return HttpResponse(template.render(context, request))
 
